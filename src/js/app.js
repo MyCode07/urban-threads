1fe3cc562ea3c83dcb6159d3345b5271a390d872
MyCode07 <erem19992015@mail.ru>
2023-08-17 12:09:25 +0300

    index

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 import './utils/menu.js'; 
 import './utils/ticker.js';
+import './utils/replace.js';
+import './utils/sliders.js';
 
 // import './utils/smoothscrol.js';
 // import './utils/animations.js';
@@ -10,8 +12,6 @@ import './utils/ticker.js';
 // import './utils/inputmask.js';
 // import './utils/forms.js';
 // import './utils/accordeon.js';
-// import './utils/replace.js';
-// import './utils/sliders.js';
 // import './utils/lazyImages.js';
 
 
--- src/js/utils/sliders.js
+++ src/js/utils/sliders.js
@@ -1,22 +1,30 @@
-import { Swiper, Lazy, Pagination, Navigation } from "swiper";
+import { Swiper, Lazy, Navigation, Autoplay } from "swiper";
 
-const slider = document.querySelector('..swiper');
-const sliderSlides = document.querySelectorAll('.swiper .swiper-slide');
+const slider = document.querySelector('.bestsellers__slider');
+const sliderSlides = document.querySelectorAll('.bestsellers__slider .swiper-slide');
 
 if (sliderSlides.length) {
     new Swiper(slider, {
         modules: [
-            Lazy, Pagination, Navigation
+            Lazy, Navigation, Autoplay
         ],
-        loop: true,
         slidesPerView: 'auto',
-
+        spaceBetween: 47,
         preloadImages: false,
+        autoplay: true,
+        speed: 1000,
+        grabCursor: true,
+        mousewheelControl: true,
         lazy: {
             loadPrevNext: true,
             loadOnTransitionStart: false
         },
+        autoplay:
+        {
+            delay: 1,
+        },
         watchSlidesProgress: true,
         watchOverflow: true,
+
     })
 }
